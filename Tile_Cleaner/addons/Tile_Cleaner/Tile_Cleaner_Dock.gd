186d629149eb0b684e64eae8ecea6c0ed115b755
marooncarl <marooncarl@gmail.com>
2019-10-25 16:29:57 -0400

    Save dialog prints a message if you don't have an autotile setup scene open

--- Tile_Cleaner/addons/Tile_Cleaner/Tile_Cleaner_Dock.gd
+++ Tile_Cleaner/addons/Tile_Cleaner/Tile_Cleaner_Dock.gd
@@ -6,7 +6,7 @@ tool
 extends Panel
 
 var editor_interface : EditorInterface = null
-# Rules to be saved
+# Rules to be saved; cleared after saving
 var rules := []
 
 func _ready():
@@ -14,10 +14,13 @@ func _ready():
 	$Save_File_Dialog.connect("file_selected", self, "on_save_file_selected")
 
 func on_save_pressed():
-	var setup = editor_interface.get_edited_scene_root()
-	if setup.has_method("create_autotile_rules"):
-		rules = setup.create_autotile_rules()
-		$Save_File_Dialog.popup_centered()
+	if editor_interface:
+		var setup = editor_interface.get_edited_scene_root()
+		if setup && setup.has_method("create_autotile_rules"):
+			rules = setup.create_autotile_rules()
+			$Save_File_Dialog.popup_centered()
+		else:
+			print("Open an Autotile Setup to save rules!")
 
 func on_save_file_selected(path: String):
 	var ruleset = AutotileRuleset.new()
