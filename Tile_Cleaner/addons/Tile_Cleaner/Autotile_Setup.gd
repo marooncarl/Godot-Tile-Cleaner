9b056f9c767104ec95d76b2c0a9333c5178a1a51
marooncarl <marooncarl@gmail.com>
2019-10-25 18:11:05 -0400

    Autotile setup adds input data to the ruleset from the input tilemap.

--- Tile_Cleaner/addons/Tile_Cleaner/Autotile_Setup.gd
+++ Tile_Cleaner/addons/Tile_Cleaner/Autotile_Setup.gd
@@ -1,4 +1,5 @@
 # Autotile Setup
+#
 # Creates autotile rules using child tilemaps
 
 tool
@@ -22,18 +23,39 @@ func create_autotile_rules() -> Array:
 		regions_map = $Regions
 	elif has_node("regions"):
 		regions_map = $regions
+	var input_map : TileMap = null
+	if has_node("Input"):
+		input_map = $Input
+	elif has_node("input"):
+		input_map = $input
 	
 	if !regions_map:
 		print("Missing regions map!")
 		return []
+	if !input_map:
+		print("Missing input map!")
+		return []
 	
-	# Determine region
+	# Determine regions
 	var regions := []
 	for cell in regions_map.get_used_cells():
 		if !is_cell_in_any_region(cell, regions):
 			var new_region := {}
 			regions.append(new_region)
 			gather_cell(cell, new_region, regions, regions_map)
+	
+	# Add input data
+	for region in regions:
+		for cell in region.keys():
+			region[cell]["input"] = {
+				"id": input_map.get_cell(cell.x, cell.y),
+				"x_flip": input_map.is_cell_x_flipped(cell.x, cell.y),
+				"y_flip": input_map.is_cell_y_flipped(cell.x, cell.y),
+				"transpose": input_map.is_cell_transposed(cell.x, cell.y),
+			}
+			# In input, empty should be regarded as a wildcard
+			if region[cell]["input"]["id"] == TileMap.INVALID_CELL:
+				region[cell]["input"]["id"] = "any"
 	
 	return regions
 
