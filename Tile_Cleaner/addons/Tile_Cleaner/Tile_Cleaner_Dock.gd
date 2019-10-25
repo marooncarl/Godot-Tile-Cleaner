aeba9de290b7eb0db768cd96436d016c20d08ba6
marooncarl <marooncarl@gmail.com>
2019-10-25 16:40:25 -0400

    Tile cleaner dock asks for the rules to be created right when saving them, instead of when pressing the save button.

--- Tile_Cleaner/addons/Tile_Cleaner/Tile_Cleaner_Dock.gd
+++ Tile_Cleaner/addons/Tile_Cleaner/Tile_Cleaner_Dock.gd
@@ -6,24 +6,29 @@ tool
 extends Panel
 
 var editor_interface : EditorInterface = null
-# Rules to be saved; cleared after saving
-var rules := []
 
 func _ready():
 	$Save_Button.connect("pressed", self, "on_save_pressed")
 	$Save_File_Dialog.connect("file_selected", self, "on_save_file_selected")
 
 func on_save_pressed():
+	# Make sure a ruleset can be saved before bringing up the save dialog
 	if editor_interface:
 		var setup = editor_interface.get_edited_scene_root()
 		if setup && setup.has_method("create_autotile_rules"):
-			rules = setup.create_autotile_rules()
 			$Save_File_Dialog.popup_centered()
 		else:
 			print("Open an Autotile Setup to save rules!")
 
 func on_save_file_selected(path: String):
-	var ruleset = AutotileRuleset.new()
-	ruleset.rules = rules
-	ResourceSaver.save(path, ruleset)
-	rules = []
+	if editor_interface:
+		var setup = editor_interface.get_edited_scene_root()
+		if setup && setup.has_method("create_autotile_rules"):
+			# Actually save the ruleset
+			var ruleset = AutotileRuleset.new()
+			ruleset.rules = setup.create_autotile_rules()
+			ResourceSaver.save(path, ruleset)
+			return
+	
+	# Didn't return, so there was an error
+	print("Failed to save autotile rules")
