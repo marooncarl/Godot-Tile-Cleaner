6def97b6ce676205a2da4da2ec61075f7f8d8ca6
marooncarl <marooncarl@gmail.com>
2019-10-31 18:13:04 -0400

    Bitmask editor shows a single tile (or a group of autotiles)

--- Tile_Cleaner/addons/Tile_Cleaner/Bitmask_Edges_Editor.gd
+++ Tile_Cleaner/addons/Tile_Cleaner/Bitmask_Edges_Editor.gd
@@ -5,8 +5,17 @@ extends Control
 
 export(TileSet) var tileset
 
+var current_id := 0
+
+onready var tile := $Sprite_Container/Tile
+
 
 func _ready():
 	if tileset:
 		var first_id = tileset.get_tiles_ids()[0]
-		$Tile.texture = tileset.tile_get_texture(first_id)
+		set_current_tile(first_id)
+
+func set_current_tile(new_id):
+	current_id = new_id
+	tile.texture = tileset.tile_get_texture(current_id)
+	tile.region_rect = tileset.tile_get_region(current_id)
