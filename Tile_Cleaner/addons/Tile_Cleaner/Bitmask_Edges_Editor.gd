cd73c1aadc382b293527fc0bde16c98da3041aa0
marooncarl <marooncarl@gmail.com>
2019-10-31 18:56:52 -0400

    Bitmask editor skips autotile ids, only showing single tiles.

--- Tile_Cleaner/addons/Tile_Cleaner/Bitmask_Edges_Editor.gd
+++ Tile_Cleaner/addons/Tile_Cleaner/Bitmask_Edges_Editor.gd
@@ -14,39 +14,65 @@ onready var id_label := $ID_Selector/ID_Label
 func _ready():
 	if tileset:
 		var first_id = tileset.get_tiles_ids()[0]
+		if tileset.tile_get_tile_mode(first_id) != TileSet.SINGLE_TILE:
+			first_id = get_next_id()
 		set_current_tile(first_id)
+		id_label.text = str(first_id)
 	
 	# Connect button signals
 	$ID_Selector/Next_ID_Button.connect("pressed", self, "next_id_pressed")
 	$ID_Selector/Prev_ID_Button.connect("pressed", self, "prev_id_pressed")
 
 func set_current_tile(new_id):
+	assert tileset
 	current_id = new_id
 	tile.texture = tileset.tile_get_texture(current_id)
 	tile.region_rect = tileset.tile_get_region(current_id)
 
+# Returns next non-autotile id.
+# If it loops around the whole list, returns the same id as current.
 func get_next_id() -> int:
+	assert tileset
 	var ids : Array = tileset.get_tiles_ids()
-	var id_index = ids.find(current_id)
-	id_index += 1
-	if id_index >= ids.size():
-		id_index = 0
+	var id_index : int = ids.find(current_id)
+	
+	var iterations := 0
+	while iterations < ids.size():
+		iterations += 1
+		id_index += 1
+		if id_index >= ids.size():
+			id_index = 0
+		if tileset.tile_get_tile_mode(ids[id_index]) == TileSet.SINGLE_TILE:
+			break
+	
 	return ids[id_index]
 
+# Returns previous non-autotile id.
+# If it loops around the whole list, returns the same id as current.
 func get_prev_id() -> int:
+	assert tileset
 	var ids : Array = tileset.get_tiles_ids()
 	var id_index = ids.find(current_id)
-	id_index -= 1
-	if id_index < 0:
-		id_index = ids.size() - 1
+	
+	var iterations := 0
+	while iterations < ids.size():
+		iterations += 1
+		id_index -= 1
+		if id_index < 0:
+			id_index = ids.size() - 1
+		if tileset.tile_get_tile_mode(ids[id_index]) == TileSet.SINGLE_TILE:
+			break
+	
 	return ids[id_index]
 
 # Button events
 
 func next_id_pressed():
-	set_current_tile(get_next_id())
-	id_label.text = str(current_id)
+	if tileset:
+		set_current_tile(get_next_id())
+		id_label.text = str(current_id)
 
 func prev_id_pressed():
-	set_current_tile(get_prev_id())
-	id_label.text = str(current_id)
+	if tileset:
+		set_current_tile(get_prev_id())
+		id_label.text = str(current_id)
