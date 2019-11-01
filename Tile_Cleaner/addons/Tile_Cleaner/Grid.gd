fe48a669b86963e79d541af4c07cf321162296fb
marooncarl <marooncarl@gmail.com>
2019-11-01 17:25:13 -0400

    Setting the grid's origin changes the offset of the grid

--- Tile_Cleaner/addons/Tile_Cleaner/Grid.gd
+++ Tile_Cleaner/addons/Tile_Cleaner/Grid.gd
@@ -9,7 +9,10 @@ export(Vector2) var size := Vector2(16, 16) setget size_set
 export(Vector2) var sub_cells := Vector2(1, 1) setget sub_cells_set
 export(Color) var grid_color := Color.white
 export(Color) var sub_cell_color := Color.gray
-export(Vector2) var origin := Vector2()
+
+# Relative to the grid's position
+export(Vector2) var origin := Vector2() setget origin_set
+
 
 func size_set(new_size: Vector2):
 	size = new_size
@@ -19,23 +22,33 @@ func sub_cells_set(new_sub_cells: Vector2):
 	sub_cells = new_sub_cells
 	update()
 
+func origin_set(new_origin: Vector2):
+	origin = new_origin
+	update()
+
 func _draw():
 	if size.y > 0:
 		var row_lines := rect_size.y / size.y + 1
+		var start_y = fmod(origin.y, size.y)
 		for row in range(0, row_lines):
-			draw_line(Vector2(0, row * size.y), Vector2(rect_size.x, row * size.y), grid_color)
+			var line_y = start_y + row * size.y
+			if line_y < rect_size.y:
+				draw_line(Vector2(0, line_y), Vector2(rect_size.x, line_y), grid_color)
 			if sub_cells.y > 1.0:
 				for sub_cell in range(1, sub_cells.y + 1.0):
-					var line_y = row * size.y + sub_cell * (size.y / sub_cells.y)
+					line_y = start_y + row * size.y + sub_cell * (size.y / sub_cells.y)
 					if line_y < rect_size.y:
 						draw_line(Vector2(0, line_y), Vector2(rect_size.x, line_y), sub_cell_color)
 	
 	if size.x > 0:
 		var column_lines := rect_size.x / size.x + 1
+		var start_x = fmod(origin.x, size.x)
 		for column in range(0, column_lines):
-			draw_line(Vector2(column * size.x, 0), Vector2(column * size.x, rect_size.y), grid_color)
+			var line_x = start_x + column * size.x
+			if line_x < rect_size.x:
+				draw_line(Vector2(line_x, 0), Vector2(line_x, rect_size.y), grid_color)
 			if sub_cells.x > 1.0:
 				for sub_cell in range(1, sub_cells.x + 1.0):
-					var line_x = column * size.x + sub_cell * (size.x / sub_cells.x)
+					line_x = start_x + column * size.x + sub_cell * (size.x / sub_cells.x)
 					if line_x < rect_size.x:
 						draw_line(Vector2(line_x, 0), Vector2(line_x, rect_size.y), sub_cell_color)
